afc3f74e78a70dbc0c0f1d530f0a42df97889efe
Ashab-Asir <ashabasir101@gmail.com>
2025-06-25 18:31:50 +0600

    fix: switch color system from hsl to oklch to enable dark mode functionality

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import RootLayout from "@/layouts/RootLayout";
 import HomePage from "@/pages/HomePage";
+import RootErrorBoundary from "@/pages/RootErrorBoundary";
 import { createBrowserRouter } from "react-router-dom";
 import type { RouteObject } from "react-router-dom";
 const rootRouteChildren: RouteObject[] = [
@@ -11,6 +12,7 @@ const rootRouteChildren: RouteObject[] = [
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <RootErrorBoundary></RootErrorBoundary>,
     element: <RootLayout></RootLayout>,
     children: rootRouteChildren,
   },
